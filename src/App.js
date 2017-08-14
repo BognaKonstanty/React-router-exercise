2ad507e8faff81f955174edd70a330cdb0899da3
Bogna Konstanty <rosiakb@gmail.com>
2017-08-14 15:46:15 +0200

    correction

--- src/App.js
+++ src/App.js
@@ -1,16 +1,49 @@
 import React, { Component } from 'react';
-import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import { Router, Route,Link, IndexRoute, hashHistory } from 'react-router';
 
-class App extends Component {
+ class App extends Component {
  render() {
    return (
      <Router history={hashHistory}>
-       <Route path='/' component={Home} />
-       <Route path='/contact' component={Contact} />
+       	<Route path='/' component={Navigation} >
+       		<IndexRoute component={Home} />
+       		<Route path='/contact' component={Contact} />
+       </Route>	
      </Router>
    )
  }
 }
+
+const Navigation = props => (
+    <div>
+       <ul>
+            <Link to="/" activeStyle={{color: '#48abee'}} onlyActiveOnIndex>Home</Link>
+			<Link to="/contact" activeStyle={{color: '#48abee'}} onlyActiveOnIndex>Contact</Link>
+        </ul>
+        {props.children}
+    </div>
+);
+
+const Hello = (props) => <h1>Witaj, {props.params.name}</h1>;
+
+<Router history={hashHistory}>
+    <Route path="/" component={Navigation}>
+            <IndexRoute component={Home} />
+            <Route path='/contact' component={Contact} />
+    <Route path='/hello/:name' component={Hello} />
+    <Route path='*' component={PageNotFound} />
+    </Route>
+</Router>
+
+const PageNotFound = () => <h1>404 Not Found</h1>;
+<Router history={hashHistory}>
+        <Route path="/" component={Navigation}>
+                <IndexRoute component={Home} />
+                <Route path='/contact' component={Contact} />
+        <Route path='*' component={PageNotFound} />
+        </Route>
+</Router>
+
 const Home = () => <h1>Hej, tu Home component</h1>
 const Contact = () => <h1>A tu Contact component</h1>
 export default App;
